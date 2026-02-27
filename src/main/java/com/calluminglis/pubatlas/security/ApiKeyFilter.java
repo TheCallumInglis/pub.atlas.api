94df1bc01111cddf8756e8e1695ad5f45cc094de
Callum Inglis <callum.inglis@4oh4.co.uk>
2026-02-27 22:12:26 +0000

    feat: Adds (very) basic auth

---
+++ src/main/java/com/calluminglis/pubatlas/security/ApiKeyFilter.java
@@ -0,0 +1,33 @@
+package com.calluminglis.pubatlas.security;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import jakarta.annotation.Priority;
+import jakarta.ws.rs.Priorities;
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+@Priority(Priorities.AUTHENTICATION)
+public class ApiKeyFilter implements ContainerRequestFilter {
+
+    @ConfigProperty(name = "app.api.key")
+    String apiKey;
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) {
+        if (!requestContext.getMethod().equals("GET")) {
+
+            String header = requestContext.getHeaderString("X-API-Key");
+
+            if (header == null || !header.equals(apiKey)) {
+                requestContext.abortWith(
+                        Response.status(Response.Status.UNAUTHORIZED)
+                                .entity("Missing or invalid API key")
+                                .build());
+            }
+        }
+    }
+}
